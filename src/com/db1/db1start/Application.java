7c0850bb578d4f85badf09f030c2249c7f434e7b
Bruno <bruno_gobo4@hotmail.com>
2019-11-12 23:30:03 -0300

    Terminado os exercicios de texto

--- src/com/db1/db1start/Application.java
+++ src/com/db1/db1start/Application.java
@@ -1,5 +1,7 @@
 package com.db1.db1start;
 
+import org.hamcrest.core.StringEndsWith;
+
 public class Application {
 
     public Integer soma(Integer num1, Integer num2){
@@ -38,4 +40,55 @@ public class Application {
         }
         return simpar;
     }
+
+    // --------------- exercicio de texto ---------------
+    public String maiusculas(String texto){
+        return (texto.toUpperCase());
+    }
+    public String minusculas(String texto){
+        return (texto.toLowerCase());
+    }
+
+    public Integer contatexto(String texto){
+        return (texto.length());
+    }
+    public Integer contatextosemespaco(String texto){
+        String textosemespaco = texto.replaceAll(" ","");
+        return (textosemespaco.length());
+    }
+    public String primeirasletras(String texto){
+        return (texto.substring(0,4));
+    }
+    public String terceiraemdiante(String texto){
+        return (texto.substring(3));
+    }
+    public String ultimas4(String texto){
+        return (texto.substring(texto.length()-4));
+    }
+    public String primeironome(String texto){
+        String[] a = texto.split(" ",3);
+        return ("ALUNO " + a[1] + " " + a[2] );
+    }
+    public String separartexto(String texto){
+        String[] textocortado = texto.split(", ",3);
+        return (textocortado[0] + " " + textocortado[1] + " " + textocortado[2] );
+    }
+    public Integer contarvogais(String texto){
+        int vogal=0;
+        for(int cont1 = 0; cont1 < texto.length() ; cont1++){
+            if (texto.charAt(cont1) == 'a'|texto.charAt(cont1) == 'A' |
+                    texto.charAt(cont1) == 'e'|texto.charAt(cont1) == 'E' |
+                    texto.charAt(cont1) == 'i'|texto.charAt(cont1) == 'I' |
+                    texto.charAt(cont1) == 'o'|texto.charAt(cont1) == 'O' |
+                    texto.charAt(cont1) == 'u'|texto.charAt(cont1) == 'U' ){
+                vogal++;
+            }
+        }
+        return (vogal);
+    }
+    public String invertertexto(String texto){
+        String invertido = new StringBuilder(texto).reverse().toString();
+        return (invertido);
+    }
+
 }
--- src/com/db1/db1start/Teste.java
+++ src/com/db1/db1start/Teste.java
@@ -57,6 +57,88 @@ public class Teste {
         Integer response = application.impares(22);
         Assert.assertEquals(expected, response);
     }
+    // --------------- exercicios texto ---------------
+    @Test
+    public void deveRetornarLetrasMaiusculas(){
+        Application application = new Application();
+        String expected = "ABC";
+        String response = application.maiusculas("abc");
+        Assert.assertEquals(expected, response);
+    }
+    @Test
+    public void deveRetornarLetrasMinusculas(){
+        Application application = new Application();
+        String expected = "abc";
+        String response = application.minusculas("ABC");
+        Assert.assertEquals(expected, response);
+    }
+
+    @Test
+    public void deveContarLetrasDoTexto(){
+        Application application = new Application();
+        Integer expected = 8;
+        Integer response = application.contatexto("DB1START");
+        Assert.assertEquals(expected, response);
+    }
+
+    @Test
+    public void deveContarLetrasSemEspaco(){
+        Application application = new Application();
+        Integer expected = 8;
+        Integer response = application.contatextosemespaco(" DB1START ");
+        Assert.assertEquals(expected, response);
+    }
+    @Test
+    public void deveRetornar4PrimeirasLetras(){
+        Application application = new Application();
+        String expected = "Brun";
+        String response = application.primeirasletras("Bruno Gobo Soares");
+        Assert.assertEquals(expected, response);
+    }
+     @Test
+    public void deveRetornarApartirDaTerceiraLetra(){
+        Application application = new Application();
+        String expected = "no Gobo Soares";
+        String response = application.terceiraemdiante("Bruno Gobo Soares");
+        Assert.assertEquals(expected, response);
+    }
+     @Test
+    public void deveRetornarUltimas4Letras(){
+        Application application = new Application();
+        String expected = "ares";
+        String response = application.ultimas4("Bruno Gobo Soares");
+        Assert.assertEquals(expected, response);
+     }
+    @Test
+    public void deveSubstituirPrimeiroNome(){
+        Application application = new Application();
+        String expected = "ALUNO Gobo Soares";
+        String response = application.primeironome("Bruno Gobo Soares");
+        Assert.assertEquals(expected, response);
+     }
+    @Test
+    public void deveSepararOTexto(){
+        Application application = new Application();
+        String expected = "banana maçã melancia";
+        String response = application.separartexto("banana, maçã, melancia");
+        Assert.assertEquals(expected, response);
+     }
+    @Test
+    public void deveContarVogais(){
+        Application application = new Application();
+        Integer expected = 2;
+        Integer response = application.contarvogais("Atack");
+        Assert.assertEquals(expected, response);
+     }
+    @Test
+    public void deveInverterTexto(){
+        Application application = new Application();
+        String expected = "CBA";
+        String response = application.invertertexto("ABC");
+        Assert.assertEquals(expected, response);
+     }
+
+
 
 
 }
