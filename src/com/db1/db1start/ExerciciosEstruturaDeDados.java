0ee0da884445ecaf17ac62043896131ea118a42e
Bruno <Bruno_gobo4@hotmail.com>
2019-11-18 13:58:18 -0300

    Corrigido camelCase

--- src/com/db1/db1start/ExerciciosEstruturaDeDados.java
+++ src/com/db1/db1start/ExerciciosEstruturaDeDados.java
@@ -4,20 +4,19 @@ import java.util.*;
 
 public class ExerciciosEstruturaDeDados {
     public static void main(String[] args) {
-
         //Método que retorne os nomes das cores que você mais gosta
-        List<String> cores = criarlistadetres("Azul", "Amarelo", "Vermelho");
+        List<String> cores = criarListaDeTres("Azul", "Amarelo", "Vermelho");
         imprimirLista("Imprimindo as cores: ", cores);
         separar();
 
         //Método que dado uma lista retorne a quantidade de itens
-        tamanhodalista(cores);
+        tamanhoDaLista(cores);
         separar();
 
         //Método que receba 3 String, adicione todos em uma lista e remova a segunda posição
-        List<String> listaqualquer = criarlistadetres("Item1", "Item2", "Item3");
+        List<String> listaqualquer = criarListaDeTres("Item1", "Item2", "Item3");
         imprimirLista("Imprimindo a lista de itens completa: " , listaqualquer);
-        imprimirLista("Imprimindo a lista de itens sem o segundo item: " , removersegundoitem(listaqualquer));
+        imprimirLista("Imprimindo a lista de itens sem o segundo item: " , removerSegundoItem(listaqualquer));
         separar();
 
         //Método que imprima a lista de cores do primeiro método
@@ -29,14 +28,14 @@ public class ExerciciosEstruturaDeDados {
         separar();
 
         //Método que receba uma lista das cores que você mais gosta e o nome de uma cor a ser removida
-        List<String> outrascores = criarlistadetres("Azul", "Verde", "Magenta");
+        List<String> outrascores = criarListaDeTres("Azul", "Verde", "Magenta");
         imprimirLista("A lista completa é: " , outrascores);
-        removeritempornome(outrascores, "Magenta");
+        removerItemPorNome(outrascores, "Magenta");
         imprimirLista("A lista sem a cor 'magenta' fica: ", outrascores);
         separar();
 
         //Método que receba a lista de cores que você gosta e imprima em ordem decrescente (alfabética)
-        ordenardescrescentelistas(cores);
+        ordenarDescrescenteListas(cores);
         imprimirLista("Lista em ordem decescente " , cores);
         separar();
 
@@ -44,41 +43,35 @@ public class ExerciciosEstruturaDeDados {
         List<Integer> numeros = new ArrayList<>();
         numeros.addAll(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
 
-        Map<Integer, String> mapanumeros = mapaparimpar(numeros);
-        imprimirmapa("O mapa que mostra numeros e suas propriedades fica: ", mapanumeros);
+        Map<Integer, String> mapanumeros = mapaParImpar(numeros);
+        imprimirMapa("O mapa que mostra numeros e suas propriedades fica: ", mapanumeros);
 
     }
 
-    private static Map<Integer, String> mapaparimpar(List<Integer> numeros) {
-        Map<Integer, String> mapanumeros = new HashMap<>();
+    private static Map<Integer, String> mapaParImpar(List<Integer> numeros) {
+        Map<Integer, String> mapaNumeros = new HashMap<>();
         for (int c = 0; c <= 10 ; c++){
             if (numeros.get(c) % 2 == 0){
-                mapanumeros.put(c , "Par");
+                mapaNumeros.put(c , "Par");
             }else{
-                mapanumeros.put(c , "Impar");
+                mapaNumeros.put(c , "Impar");
             }
         }
-        return mapanumeros;
+        return mapaNumeros;
     }
 
-    private static void imprimirmapa(String texto, Map<Integer, String> mapa) {
+    private static void imprimirMapa(String texto, Map<Integer, String> mapa) {
         System.out.println(" ");
         System.out.println(texto);
         System.out.println(mapa);
         System.out.println(" ");
     }
 
-    private static void listanumeros10() {
-        List<Integer> numeros = new ArrayList<>();
-        numeros.addAll(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
-    }
-
-
-    private static void tamanhodalista(List<String> cores) {
+    private static void tamanhoDaLista(List<String> cores) {
         System.out.println("A lista tem " + cores.size() +" itens.");
     }
 
-    private static List<String> criarlistadetres(String item1, String item2, String item3) {
+    private static List<String> criarListaDeTres(String item1, String item2, String item3) {
         List<String> lista = new ArrayList<>();
         lista.add(item1);
         lista.add(item2);
@@ -86,7 +79,7 @@ public class ExerciciosEstruturaDeDados {
         return lista;
     }
 
-    private static List<String> removersegundoitem(List<String> lista) {
+    private static List<String> removerSegundoItem(List<String> lista) {
         lista.remove(1);
         return lista;
     }
@@ -100,11 +93,11 @@ public class ExerciciosEstruturaDeDados {
         Collections.sort(lista);
         return (lista);
     }
-    private static void removeritempornome(List<String> lista, String itempararemover) {
+    private static void removerItemPorNome(List<String> lista, String itempararemover) {
         lista.remove(itempararemover);
     }
 
-    private static void ordenardescrescentelistas(List<String> cores) {
+    private static void ordenarDescrescenteListas(List<String> cores) {
         Collections.reverse(ordenarlista(cores));
     }
     private static void separar(){
